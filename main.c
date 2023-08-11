a0d23279c53ae6da20f8eef81549d66d21f9ce77
linuxu <linuxu@ParallelC23-1.academic.management.afeka.local>
2023-08-11 21:32:55 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <omp.h>
+#include "point.h"
+#include "proximity_utils.h"
+#include "cuda_utils.h"
+
+int main(int argc, char* argv[]) {
+    // Initialize MPI
+    MPI_Init(&argc, &argv);
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (size != 2) {
+        printf("This code is designed for two processes.\n");
+        MPI_Finalize();
+        return 1;
+    }
+
+    if (rank == 0) {
+        // Master process reads input data and sends work to workers
+        int N, K, tCount;
+        double D;
+        Point* points;
+
+        readInputData("input.txt", &N, &K, &D, &tCount, &points);
+
+        // Distribute input points and t values to workers
+        // Use MPI_Send and MPI_Recv for communication
+        
+        // Free allocated memory
+        free(points);
+    } else if (rank == 1) {
+        // Worker process
+        int N;
+        Point* points;
+        double t = 0.0; // Placeholder for t value
+
+        // Receive input points and t values from the master
+        
+        // Perform GPU-accelerated computation using CUDA
+        performGPUComputation(points, N, t);
+
+        // Use OpenMP to parallelize Proximity Criteria check
+        // Ensure thread safety for accessing shared data structures
+        
+        // Send computed results back to the master using MPI_Send
+        
+        // Free allocated memory
+        free(points);
+    }
+
+    // Finalize MPI
+    MPI_Finalize();
+
+    return 0;
+}

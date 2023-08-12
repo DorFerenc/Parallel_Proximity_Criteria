ecbef9a35a2b929f14dcffc5aefc9942d2b46a7a
linuxu <linuxu@ParallelC23-9.academic.management.afeka.local>
2023-08-12 16:16:00 +0300

    1.6

--- myProto.h
+++ myProto.h
@@ -3,3 +3,4 @@
 #include "point.h"
 
 int performGPUComputation(Point* points, int N, double* tValues);
+void testCoordinates(Point* originalPoints, Point* computedPoints, int numPoints, double* tValues, int tCount);
